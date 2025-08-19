a0bce5623ba3f5d7fe097ccad39f7d16b5cc7672
Himanshu Ranjan <hr7319776084@gmail.com>
2025-08-20 03:03:48 +0530

    inngest init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-
-import { Button } from "@/components/ui/button"
-import prisma from "@/lib/db"
-import { useTRPC } from "@/trpc/client"
-import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
-
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
 import { caller ,trpc} from "@/trpc/server"
 import {getQueryClient} from "@/trpc/server"
@@ -13,19 +8,12 @@ import { Suspense } from "react"
 export default async function Home() {
 
   const queryClient=getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"antonio prefetch"}))  //prefetch query
-
-  const data= await caller.hello({text:"antonio"})
- 
-
-
-
-  
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"antonio prefetch"}))  //prefetch query , void cuz it doesnt return anything , it just prefetch the query
 
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<p>Loading...</p>}>
     <Client/>
     </Suspense>
     </HydrationBoundary>
